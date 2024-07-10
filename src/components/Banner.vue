3994b5fc4e635429380bed6879572b41cd6514cb
wilber <parionahuaraca@gmail.com>
2024-07-10 00:40:11 -0500

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,143 @@
+<template>
+<v-container class="banner banner-content"  id="Acercademi"> 
+
+  <v-layout row wrap>  
+    <v-flex xs12 sm12 md4 lg5 xl4 >  
+      <div class="banner-bio">
+        <h2>¡Hola! Soy Wilber.</h2>
+        <p>
+          Un desarrollador web con 4 años de experiencia en el maravilloso mundo de WordPress. Aunque amo WordPress, estoy listo para un nuevo desafío y quiero sumergirme en el universo de React como Desarrollador.
+        <br> <br>
+          Me encanta aprender nuevas tecnologías y adaptarme a las necesidades del proyecto. He estado trabajando en mejorar mis habilidades en React, y estoy emocionado por la oportunidad de contribuir a un equipo donde pueda crecer y aportar con mi pasión por el desarrollo web. ¡Estoy listo para esta nueva aventura!
+
+        </p>
+      </div>
+       
+<!--       <v-btn class="banner-btn large fill-bg  "  round color="secondary"   data-aos="fade-up" outline  data-aos-offset="200" data-aos-delay="800"  data-aos-once="true" href="https://drive.google.com/file/d/1_UhUSBTwYFE0t8eqjyy2rh81qGKtUBGg/view" target="_blank"   >Descargar CV </v-btn>
+ -->      <v-btn class="banner-btn  large banner-btn--whatsapp" dark="false" round  color="secondary"    outline data-aos="fade-up"  data-aos-offset="200" data-aos-delay="1200" data-aos-once="true" href="https://api.whatsapp.com/send?phone=51938343796&text=Hola" target="_blank" > WhatsApp </v-btn>
+      <div class="banner-avatar d-inline-flex py-3 px-4" outline data-aos="fade-up"  data-aos-offset="200" data-aos-delay="1500"  >
+        <div  class="banner-avatar-image" >
+          <img  class=" " src="../assets/wilberpariona.jpg" alt="Github"> 
+              <v-btn class="mx-2 social-icon  " href="https://github.com/WilberCode" target="_blank" >
+                <v-icon size="18px"> {{'fab fa-github'}} </v-icon>
+             </v-btn> 
+        </div>
+        <div class="d-flex  align-center" >
+           <div class="ml-4">
+             <p class="mb-2 font-weight-bold banner-avatar-number	">Teléfono: <a href="tel:+51926776516">926776516</a></p>  
+             <p  class="mb-0 " >Lima - Perú</p> 
+           </div>
+        </div>
+      </div>
+    </v-flex>
+    <v-flex xs12 sm12 md8 lg7 xl8>  
+      <div class=" banner-image text-sx-center relative text-sm-center text-md-center text-lg-right" >
+        <img :src="require('../assets/me2.svg')" data-aos="fade-up"  data-aos-offset="200"  data-aos-once="true" data-aos-delay="1800" >
+      </div>
+      </v-flex>
+  </v-layout> 
+</v-container>
+</template>
+<script>
+
+export default {
+    data(){
+      return{
+     
+      }
+    } 
+}
+</script>
+<style lang="scss">
+ 
+$md: 959px; 
+.banner{
+  &-content{
+    position:relative;
+    z-index: 5;
+  }
+  &-bio{
+    h2{
+      margin-top:1em;
+      margin-bottom:.8em;
+    }
+    color:#fff;
+  }
+  
+  &-btn{
+    font-size:14px;
+    color:#ffffff;
+    text-transform:none;
+    padding:1.8rem 2em;
+    margin-left:0;  
+    border-radius: 14px;
+    font-size: 18px;    
+    &--whatsapp{
+      border-width: 2px !important; 
+    }
+    @media (max-width:600px) {
+      padding:1.8rem 1.5em; 
+      
+    }
+  }
+  & .fill-bg.v-btn.v-btn--outline{
+    background: var(--v-secondary-base) !important;
+    color:white!important;
+     border-width: 2px !important;
+     border-color: var(--v-secondary-base) ;
+  }
+  &-avatar{
+
+    box-shadow: var(--shadow-lg);
+    border-radius: 14px;
+    overflow: hidden;
+    background: #fff; 
+    width: 100%;
+    max-width: 350px;
+    margin-top: 2em;
+    &-image{
+      flex: initial !important; 
+      position: relative;
+      .social-icon {
+          width: 38px !important;
+          height: 38px !important;
+          position: absolute;
+          bottom: -15px;
+          left: -8px;
+          background: #000;
+          display: inline-flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 100%;
+          min-width: auto;
+          border:2px solid #000; 
+           box-shadow: none !important; 
+           background-color: #fff !important;
+      }
+    }
+    &-number{
+      font-size: 15px;
+    }
+    img{
+      width: 80px; 
+      display: block;
+      border-radius: 50%;
+      background:var(--v-primary-lighten5);
+    }
+  }
+  &-image{
+    margin-top:2em ; 
+    img{
+      @media (max-width:$md){
+        width:50%; 
+      }
+    //  @media (max-width:var(--sm)){
+    //     width:20%; 
+    //   }
+    }
+   @media (min-width:360px) and  (max-width:645px) {
+      margin-top:7em;
+   }
+  }
+}
+</style>
---
+++ src/components/Success.vue
@@ -0,0 +1,161 @@
+<template>
+<v-container grid-list-md mb-4 >
+  <v-layout row  wrap align-content-space-between="true" class="card-wrap" >
+    <template v-for="page in pages">  
+      <v-flex xs12 sm12 md6 lg6  px-3 py-3  :key="page.sertor" data-aos="fade-up"  data-aos-offset="200"  data-aos-once="true"> 
+        <v-card class="card">
+          <v-img
+            class="white--text" 
+            :src="require(`../assets/${page.imgUrl}`)" 
+          >
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <!-- <span class="headline">Top 10 Australian beaches</span> -->
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <div class="card-description">
+              <v-card-title >
+              <div>
+                <span class="font-weight-bold">{{page.name}}</span> 
+                <p class="grey--text">{{page.sector}}</p> 
+                <a  class="list-link py-2 pr-5" :href="page.link" target="_blank" >{{page.link}}</a> 
+              </div>
+            </v-card-title> 
+          </div>
+        </v-card>
+      </v-flex>
+    </template>
+  </v-layout>
+</v-container>
+  
+</template>
+<script>
+export default {
+    data(){
+      return{
+        pages:[
+         
+           {
+            imgUrl:'lumiluperu.jpg',
+            name:'Lumilu Perú',
+            sector:'Market Shop', 
+            link:'https://lumiluperu.com/'
+          }, 
+          {
+            imgUrl:'pigeon.jpg',
+            name:'Pigeon LATAM',
+            sector:'Cuidado del bebé', 
+            link:'https://pigeonlatam.com'
+          },
+           {
+            imgUrl:'screenshot-igc.jpg',
+            name:'IGC - Instituto de Gerencia Intercontinental',
+            sector:'Centro de capacitación', 
+            link:'https://igc.edu.pe'
+          },
+           {
+            imgUrl:'screenshot-escuela.jpg',
+            name:'EG - Escuela de Gerencia y Gestión',
+            sector:'Centro de capacitación', 
+            link:'https://escuelag.com'
+          },
+           {
+            imgUrl:'prestigescreenshot.jpg',
+            name:'Prestige Dental Care',
+            sector:'Salud', 
+            link:'https://prestigedentalcarefl.com/'
+          },  
+         
+          {
+            imgUrl:'call2action.jpg',
+            name:'Call2action',
+            sector:'Agencia de Marketing',
+            link:'https://call2action.pe/'
+          },
+           {
+            imgUrl:'lunademielscreenshot.jpg',
+            name:'Portal de luna de miel',
+            sector:'Guía para parejas', 
+            link:'https://www.lunademiel.com.pe/'
+          },
+          {
+            imgUrl:'tendenzescreenshot.jpg',
+            name:'Tendenze Perú Comunicaciones',
+            sector:'Agencia de Marketing',
+            link:'https://tendenzeperu.com/'
+          },
+          {
+            imgUrl:'cyberweektododkasa.jpg',
+            name:'Cyber Week TododKasa',
+            sector:'Tienda', 
+            link:'https://cyberweektododkasa.feriasdigitales.pe/'
+          },
+         
+          {
+            imgUrl:'feriasdigitalesscreenshot.jpg',
+            name:'Ferias Digitales',
+            sector:'Agencia de Marketing', 
+            link:'https://feriasdigitales.pe/'
+          },
+          
+          {
+            imgUrl:'magaescreenshot.jpg',
+            name:'Il magazzino delle sorelle',
+            sector:'Decoración', 
+            link:'https://ilmagazzinodellesorelle.com/'
+          },
+           {
+            imgUrl:'nmscreenshot.jpg',
+            name:'Nuevo Mundo Espacios',
+            sector:'Inmobiliaria',
+            link:'http://www.nuevomundoespacios.com.pe/'
+          },
+          {
+            imgUrl:'meatfestscreenshot.jpg',
+            name:'Cyber Week MEAT FEST',
+            sector:'Tienda de comestibles y especialidades', 
+            link:'https://cyberweekmeatfest.feriasdigitales.pe/'
+          }, 
+          {
+            imgUrl:'cyberscreenshot.jpg',
+            name:'Cyber Week Bebe & Mamá',
+            sector:'Tienda', 
+            link:'https://cyberweekbyby.feriasdigitales.pe/'
+          },
+          {
+            imgUrl:'solidascreenshot.jpg',
+            name:'Solida Construcción',
+            sector:'Constructora', 
+            link:'https://www.solidaconstruccion.com/'
+          },
+         {
+            imgUrl:'biooilscreenshot.jpg',
+            name:'Bio Oil Perú',
+            sector:'Cuidado de la Piel', 
+            link:'https://biooilperu.pe'
+          }
+          ]
+      }
+    }
+}
+</script>
+<style lang="scss" >
+  .card{
+    &-wrap{
+      margin:0 -40px !important;
+      @media (max-width:900px){ 
+           margin:0 -34px !important;
+
+      }
+    } 
+    border-radius: 10px;
+    box-shadow: var(--shadow-lg);
+    &-description{
+      padding: 1em;
+    }
+  }
+
+</style>
